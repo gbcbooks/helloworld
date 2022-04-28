6bec3cedc61c920cfc89f4a0c23775cccebf4456
gbcbooks <gbcbooksmj@gmail.com>
2022-04-29 01:12:39 +0800

    comment somthing

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 def function1():
     print("this is function1")
 
-def function2():
-    print("this is from v0.1")
+# def function2():
+#     print("this is from v0.1")
 
 def main():
     print("helloworld")
